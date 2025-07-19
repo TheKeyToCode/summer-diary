02ce75b8e8b81576f209a7368c86d0284e06e26f
T-34 <den@a1-ru.ru>
2025-07-19 15:21:58 +0700

    almost_nothing

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <div>Если вы увидели данное сообщение это значит что проект запустился Хоят бы в начале</div>
     <App />
     <div>Если вы увидели данное сообщение это значит что проект запустился без критических ошибок.</div>
   </StrictMode>,
